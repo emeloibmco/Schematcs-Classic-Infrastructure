8a773ace4d56bf4dfab204736e09a7b644334625
acoyp99 <61330297+acoyp99@users.noreply.github.com>
2020-04-19 23:03:45 -0500

    Add files via upload

---
+++ vsi-provisioner/main.tf
@@ -0,0 +1,9 @@
+data "ibm_compute_bare_metal" "BM" {
+  hostname    = "${var.bm_hostname}"
+  domain      = "${var.bm_domain"
+  most_recent = true
+
+  provisioner "remote-exec" {
+    script = "install-viewer.sh"
+  }
+}
