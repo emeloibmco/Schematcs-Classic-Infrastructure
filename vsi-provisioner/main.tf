1b668dbbb8b9852da4616f025fef05d347baed84
acoyp99 <acoyp1999@gmail.com>
2020-04-19 23:16:30 -0500

    vsi variables

--- vsi-provisioner/main.tf
+++ vsi-provisioner/main.tf
@@ -1,6 +1,6 @@
 data "ibm_compute_bare_metal" "BM" {
   hostname    = "${var.bm_hostname}"
-  domain      = "${var.bm_domain"
+  domain      = "${var.bm_domain}"
   most_recent = true
 
   provisioner "remote-exec" {
