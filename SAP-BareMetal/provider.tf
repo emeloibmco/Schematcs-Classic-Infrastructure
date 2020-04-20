9fcc29b84235bd6c755902b63fc4b7f3089bd206
acoyp99 <61330297+acoyp99@users.noreply.github.com>
2020-04-20 09:01:38 -0500

    Update provider.tf

--- SAP-BareMetal/provider.tf
+++ SAP-BareMetal/provider.tf
@@ -1,4 +1,4 @@
-# Configuración del provider con APIKEY y la región a trabajar
+# Configuración de permisos con la  API_KEY para el aprovisionamiento de recursos
 provider ibm {
   ibmcloud_api_key      = "${var.ibmcloud_apikey}"
   region                = "${var.ibm_region}"
