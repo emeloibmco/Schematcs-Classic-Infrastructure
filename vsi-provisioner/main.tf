d53e3963e9d5346dec85ec8cba4ad54eaf2d8234
acoyp99 <acoyp1999@gmail.com>
2020-04-27 13:05:06 -0500

    provisioner

--- vsi-provisioner/main.tf
+++ vsi-provisioner/main.tf
@@ -15,7 +15,7 @@ resource "ibm_compute_vm_instance" "terraform_p_sample" {
   datacenter                 = "${var.datacenter}"
   network_speed        = 100
   hourly_billing       = true
-  private_network_only = true
+  private_network_only = false
   cores                = 1
   memory               = 1024
   disks                = [25]
