3a255b122435cb9d88e64f67a130d4358772dac0
acoyp99 <acoyp1999@gmail.com>
2020-05-18 13:48:00 -0500

    chef-install

--- Provisioner-No-resource/main.tf
+++ Provisioner-No-resource/main.tf
@@ -3,13 +3,13 @@ resource "ibm_compute_vm_instance" "terraform_p_sample" {
   domain                     = "ibm.cloud-landingzone.com"
   os_reference_code          = "${var.os_reference}"
   datacenter                 = "${var.datacenter}"
-  network_speed        = 100
-  hourly_billing       = true
-  private_network_only = false
-  cores                = 1
-  memory               = 1024
-  disks                = [25]
-  local_disk           = false
+  network_speed              = 100
+  hourly_billing             = true
+  private_network_only       = false
+  cores                      = 1
+  memory                     = 1024
+  disks                      = [25]
+  local_disk                 = false
 
   provisioner "remote-exec" {
     inline = [
