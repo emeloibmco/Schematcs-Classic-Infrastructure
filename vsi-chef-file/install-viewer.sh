2577687be3eb42faf536710bc5311bcfe41be63e
acoyp99 <acoyp1999@gmail.com>
2020-05-19 11:26:44 -0500

    chef-test

--- vsi-chef-file/install-viewer.sh
+++ vsi-chef-file/install-viewer.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 yes | sudo apt-get update
 cd ..
-touch data.txt 
-echo 'Esta es una demo provisioner remote-exec' >> data.txt
+wget -nv -P /downloads https://packages.chef.io/files/stable/chef-workstation/0.18.3/ubuntu/18.04/chef-workstation_0.18.3-1_amd64.deb
+dpkg -i /downloads/chef-workstation_0.18.3-1_amd64.deb
+echo yes | chef generate repo chef-repo
+chef generate cookbook chef-repo/cookbooks/cookbooktest
+cat <<EOT >> chef-repo/cookbooks/cookbooktest/recipes/default.rb
+file "#{ENV['HOME']}/test.txt" do
+  content 'This file was created by Chef Infra!'
+end
+EOT
+cd chef-repo
+chef-client --local-mode --override-runlist cookbooktest
--- vsi-chef-file/main.tf
+++ vsi-chef-file/main.tf
@@ -20,29 +20,7 @@ resource "ibm_compute_vm_instance" "terraform_p_sample" {
   memory               = 1024
   disks                = [25]
   local_disk           = false
-  ssh_key_ids           = [ "${ibm_compute_ssh_key.ssh_key_bin.id}" ]
+  post_install_script_uri  = "https://raw.githubusercontent.com/emeloibmco/Schematics-Classic-Infrastructure-BareMetal/master/vsi-chef-file/install-viewer.sh"
+  
 
-  connection {
-    type = "ssh"
-    user = "root"
-    private_key = "${var.private_key}"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "yes | sudo apt-get update", 
-      "cd ..",
-      "wget -nv -P /downloads https://packages.chef.io/files/stable/chef-workstation/0.18.3/ubuntu/18.04/chef-workstation_0.18.3-1_amd64.deb",
-      "dpkg -i /downloads/chef-workstation_0.18.3-1_amd64.deb",
-      "echo yes | chef generate repo chef-repo",
-      "chef generate cookbook chef-repo/cookbooks/cookbooktest",
-      "cat <<EOT >> chef-repo/cookbooks/cookbooktest/recipes/default.rb",
-      "file"+ "#{ENV['HOME']}/test.txt"+ "do",
-      "  content 'This file was created by Chef Infra!'",
-      "end",
-      "EOT",
-      "cd chef-repo",
-      "chef-client --local-mode --override-runlist cookbooktest"
-    ]
-  }
 }
