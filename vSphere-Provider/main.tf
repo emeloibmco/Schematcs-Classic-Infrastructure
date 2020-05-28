49b7b2947b59b6e34df2b2d9c858a89a9c626752
acoyp99 <acoyp1999@gmail.com>
2020-05-27 20:10:39 -0500

    Provider vSphere

--- vSphere-Provider/main.tf
+++ vSphere-Provider/main.tf
@@ -1,5 +1,13 @@
 
 provider "vsphere" {
+
+  connection {
+    type     = "ssh"
+    user     = "${var.user_name}"
+    password = "${var.tunnel_password}"
+    host     = "${var.tunnel_host}"
+  }
+
   user           = "${var.user_vsphere}"
   password       = "${var.vsphere_password}"
   vsphere_server = "${var.ip_address}"
--- vSphere-Provider/variables.tf
+++ vSphere-Provider/variables.tf
@@ -2,6 +2,18 @@ variable "ibmcloud_api_key" {
   description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys"
 } 
 
+variable "user_name" {
+
+}
+
+variable "tunnel_password" {
+
+}
+
+variable "tunnel_host" {
+  
+}
+
 variable "user_vsphere" {
 
 }
