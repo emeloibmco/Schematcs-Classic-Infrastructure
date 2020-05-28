7d455d9b528cfbcb7f466ac8773454078416fd0f
acoyp99 <acoyp1999@gmail.com>
2020-05-28 15:26:51 -0500

    Provider vSphere

--- vSphere-Provider/main.tf
+++ vSphere-Provider/main.tf
@@ -7,7 +7,45 @@
   allow_unverified_ssl = true
 
 } */
-
+/* 
 data "vsphere_datacenter" "dc" {
   name = "${var.datacenter}"
+} */
+
+resource "ibm_compute_ssh_key" "ssh_key_bin" {
+  label      = "${var.ssh_label}"
+  public_key = "${var.ssh_public_key}"
+}
+
+resource "ibm_compute_vm_instance" "terraform_p_sample" {
+  hostname                   = "${var.vsi_name}"
+  domain                     = "ibm.cloud-landingzone.com"
+  os_reference_code          = "${var.os_reference}"
+  datacenter                 = "${var.datacenter}"
+  network_speed              = 100
+  hourly_billing             = true
+  private_network_only       = false
+  cores                      = 1
+  memory                     = 1024
+  disks                      = [25]
+  local_disk                 = false
+  ssh_key_ids                = [ "${ibm_compute_ssh_key.ssh_key_bin.id}" ]
+  
+  connection {
+    type = "ssh"
+    user = "root"
+    private_key = "${var.private_key}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "yes | sudo apt-get update", 
+      "cd ..",
+      "touch data.txt", 
+      "echo 'Esta es una demo provisioner remote-exec' >> data.txt"
+    ]
+  }
+  timeouts {
+    delete = "5m"
+  }
 }
--- vSphere-Provider/variables.tf
+++ vSphere-Provider/variables.tf
@@ -2,6 +2,35 @@ variable "ibmcloud_api_key" {
   description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys"
 } 
 
+variable "ibmcloud_api_key" {
+  description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys"
+}
+
+variable "vsi_name" {
+  
+}
+
+variable "os_reference" {
+  description = ""
+  default = "UBUNTU_18_64"
+}
+
+variable "datacenter" {
+  description = ""
+  default = "dal10"
+}
+
+variable "ssh_label" {
+  default = "Personal"
+}
+
+variable "ssh_public_key" {
+}
+
+variable "private_key" {
+  
+}
+
 /* variable "user_name" {
 
 }
@@ -13,7 +42,7 @@ variable "tunnel_password" {
 variable "tunnel_host" {
 
 } */
-
+/* 
 variable "user_vsphere" {
 
 }
@@ -28,4 +57,4 @@ variable "ip_address" {
 
 variable "datacenter" {
 
-}
+} */
