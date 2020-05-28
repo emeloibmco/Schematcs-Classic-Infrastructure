a5b895be9faabbff00b623dbdd1380af9a6318ee
acoyp99 <acoyp1999@gmail.com>
2020-05-28 14:55:16 -0500

    Provider vSphere

--- vSphere-Provider/main.tf
+++ vSphere-Provider/main.tf
@@ -1,12 +1,12 @@
 
-provider "vsphere" {
+/* provider "vsphere" {
 
   user           = "${var.user_vsphere}"
   password       = "${var.vsphere_password}"
   vsphere_server = "${var.ip_address}"
   allow_unverified_ssl = true
 
-}
+} */
 
 data "vsphere_datacenter" "dc" {
   name = "${var.datacenter}"
