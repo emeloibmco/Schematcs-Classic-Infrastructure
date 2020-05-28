bf560672ab901bd0d5873fa31dacc8b804a0f0ad
acoyp99 <acoyp1999@gmail.com>
2020-05-28 15:28:29 -0500

    Provider vSphere

--- vSphere-Provider/variables.tf
+++ vSphere-Provider/variables.tf
@@ -2,12 +2,8 @@ variable "ibmcloud_api_key" {
   description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys"
 } 
 
-variable "ibmcloud_api_key" {
-  description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys"
-}
-
 variable "vsi_name" {
-  
+
 }
 
 variable "os_reference" {
