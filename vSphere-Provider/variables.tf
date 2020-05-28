ad4571f3f7b4e305aaded051d4b9e7196b32484b
acoyp99 <acoyp1999@gmail.com>
2020-05-28 15:42:14 -0500

    Provider vSphere

--- vSphere-Provider/variables.tf
+++ vSphere-Provider/variables.tf
@@ -2,31 +2,6 @@ variable "ibmcloud_api_key" {
   description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys"
 } 
 
-variable "vsi_name" {
-
-}
-
-variable "os_reference" {
-  description = ""
-  default = "UBUNTU_18_64"
-}
-
-variable "datacenter" {
-  description = ""
-  default = "dal10"
-}
-
-variable "ssh_label" {
-  default = "Personal"
-}
-
-variable "ssh_public_key" {
-}
-
-variable "private_key" {
-  
-}
-
 /* variable "user_name" {
 
 }
@@ -38,7 +13,7 @@ variable "tunnel_password" {
 variable "tunnel_host" {
 
 } */
-/* 
+
 variable "user_vsphere" {
 
 }
@@ -53,4 +28,4 @@ variable "ip_address" {
 
 variable "datacenter" {
 
-} */
+}
