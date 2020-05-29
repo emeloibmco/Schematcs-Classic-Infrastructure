4f401ed911d22ca65bdaf7d1b5069996b4ba2a15
acoyp99 <acoyp1999@gmail.com>
2020-05-28 22:33:36 -0500

    Provider vSphere

--- vSphere-Provider/main.tf
+++ vSphere-Provider/main.tf
@@ -1,44 +1,44 @@
 
 provider "vsphere" {
   
-  user           = var.user_vsphere
-  password       = var.vsphere_password
-  vsphere_server = var.ip_address
+  user           = "${var.user_vsphere}"
+  password       = "${var.vsphere_password}"
+  vsphere_server = "${var.ip_address}"
   allow_unverified_ssl = true
   
 
 } 
 
 data "vsphere_datacenter" "dc" {
-  name = var.datacenter
+  name = "${var.datacenter}"
 } 
 
 data "vsphere_datastore" "datastore" {
   name          = "datastore1"
-  datacenter_id = data.vsphere_datacenter.dc.id
-  }
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
 
 data "vsphere_resource_pool" "pool" {
-  name          = "cluster1/Resources"
-  datacenter_id = data.vsphere_datacenter.dc.id
+  name          = "Resources"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_network" "network" {
-  name          = "VM Network"
-  datacenter_id = data.vsphere_datacenter.dc.id
+  name          = "public"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 resource "vsphere_virtual_machine" "vm" {
   name             = "terraform-test"
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  datastore_id     = data.vsphere_datastore.datastore.id
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
 
   num_cpus = 2
   memory   = 1024
   guest_id = "centos8_64Guest	"
 
   network_interface {
-    network_id = data.vsphere_network.network.id
+    network_id = "${data.vsphere_network.network.id}"
   }
 
   disk {
@@ -46,3 +46,4 @@ resource "vsphere_virtual_machine" "vm" {
     size  = 20
   }
 }
+
