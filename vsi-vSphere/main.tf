4bf023fa0111664aa95f4a4dba48b5da6e753e99
acoyp99 <acoyp1999@gmail.com>
2020-05-31 11:36:33 -0500

    VM-vsphere-provider

--- vsi-vSphere/main.tf
+++ vsi-vSphere/main.tf
@@ -8,24 +8,45 @@ resource "ibm_compute_ssh_key" "ssh_key_bin" {
   public_key = "${var.ssh_public_key}"
 }
 
-resource "null_resource" "vm_connect" {
+resource "ibm_compute_vm_instance" "terraform_p_sample" {
+  hostname                   = "${var.vm_name}"
+  domain                     = "ibm.cloud-landingzone.com"
+  os_reference_code          = "${var.os_reference}"
+  datacenter                 = "${var.datacenter}"
+  network_speed        = 100
+  hourly_billing       = true
+  private_network_only = false
+  cores                = 1
+  memory               = 1024
+  disks                = [25]
+  local_disk           = false
+  ssh_key_ids           = [ "${ibm_compute_ssh_key.ssh_key_bin.id}" ]
+
+  
+
   connection {
     type = "ssh"
-    host = "169.48.173.215"
     user = "root"
     private_key = "${var.private_key}"
   }
 
   provisioner "remote-exec" {
+
+    timeout {
+    create = "20m"
+    }
+
     inline = [
-      "cd ..",
-      "wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb",
-      "sudo dpkg -i packages-microsoft-prod.deb",
-      "yes | sudo apt-get update",
-      "sudo apt-get install -y powershell",
-      "sudo pwsh",
-      "install-module -name VMware.PowerCLI -Force",
-      "Set-PowerCLIConfiguration -InvalidCertificateAction ignore -confirm:$false",
+      "sudo apt-get update",
+      "yes | sudo apt-get upgrade",
+      "curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add",
+      "sudo curl -o /etc/apt/sources.list.d/microsoft.list https://packages.microsoft.com/config/ubuntu/18.04/prod.list",
+      "sudo apt-get update",
+      "yes | sudo apt-get install powershell-preview",
+      "sudo pwsh-preview",
+      "Install-Module -Name VMware.PowerCLI -Force",
+      "Get-Module VMware.PowerCLI -ListAvailable",
+      "Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false",
       "Connect-VIServer -Server ${var.vsphere_ip} -Protocol https -User ${var.vsphere_user} -Password ${var.vsphere_passwd}",
       "New-VM -Name ${var.vm_vsphere} -VMHost ${var.vsphere_ip} -ResourcePool Resources -DiskGB ${var.vsphere_disk} -DiskStorageFormat Thin -DataStore datastore1 -MemoryGB ${var.vsphere_memory}",
 
