bb32b372a618e1bbe3298aaf1c82d0175f478b52
acoyp99 <acoyp1999@gmail.com>
2020-06-01 02:51:11 -0500

    VM-vsphere-provider

--- null-resource/main.tf
+++ null-resource/main.tf
@@ -6,25 +6,20 @@ provider "ibm" {
 
 resource "null_resource" "win_vm" {
 
-  provisioner "local-exec" {
-    command = "wget https://support.arraynetworks.net/prx/001/http/supportportal.arraynetworks.net/downloads/pkg_9_4_0_322/MP_Linux_1.2.6/MotionPro_Linux_Ubuntu_x64_v1.2.6.sh"
-     
-  }
-  provisioner "local-exec" {
-    command = "ls"
-     
-  }
-  provisioner "local-exec" {
-    command = "chmod +x MotionPro_Linux_Ubuntu_x64_v1.2.6.sh"
-     
-  }
-  provisioner "local-exec" {
-    command = "./MotionPro_Linux_Ubuntu_x64_v1.2.6.sh"
-     
-  }
-  provisioner "local-exec" {
-    command = "ls"
-     
+  connection {
+    host = "169.48.173.214"
+    type = "ssh"
+    user = "root"
+    private_key = "${var.private_key}"
   }
 
+  provisioner "remote-exec" {
+    inline = [
+      "git clone https://github.com/emeloibmco/Schematics-Classic-Infrastructure-BareMetal.git",
+      "ls",
+      "cd Schematics-Classic-Infrastructure-BareMetal/vSphere-Provider",   
+      "ls",   
+      "terraform init"
+    ]
+  }
 }
