3d15a9db3e7d6e5b16739db9b1b08075864a8fab
acoyp99 <acoyp1999@gmail.com>
2020-06-01 11:18:27 -0500

    VM-vsphere-provider

---
+++ local-exec/main.tf
@@ -0,0 +1,9 @@
+resource "null_resource" "keys" {
+
+
+  provisioner "remote-exec" {
+    inline = [
+      "ssh-keygen -m PEM -t rsa -f "NOMBRE_DE_LA_LLAVE""
+    ]
+  }
+}
--- local-exec/vars.tf
+++ local-exec/vars.tf
@@ -2,12 +2,4 @@ variable "ibmcloud_api_key" {
   description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys"
 }
 
-variable "os_reference" {
-  description = ""
-  default = "UBUNTU_18_64"
-}
 
-variable "datacenter" {
-  description = ""
-  default = "dal10"
-}
