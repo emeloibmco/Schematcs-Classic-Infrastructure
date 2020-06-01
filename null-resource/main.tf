1c10af500897b937c4754b0684234baee0438a38
acoyp99 <acoyp1999@gmail.com>
2020-06-01 09:29:30 -0500

    VM-vsphere-provider

--- null-resource/main.tf
+++ null-resource/main.tf
@@ -7,21 +7,14 @@ provider "ibm" {
 resource "null_resource" "win_vm" {
 
   connection {
-    host = "169.48.173.214"
+    host = "169.62.137.214"
     type = "ssh"
     user = "root"
-    private_key = "${var.private_key}"
+    password = "a62T2QME"
   }
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt-get update",
-      "sudo apt-get upgrade",
-      "cd ..",
-      "mkdir terraform && cd terraform",
-      "wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip"
-      "unzip terraform_0.11.14_linux_amd64.zip",
-      "export PATH=$PATH:$HOME/terraform",
       "terraform"
     ]
   }
