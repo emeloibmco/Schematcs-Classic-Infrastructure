5d833a77908afafb03925bf502e3b67203d53f38
acoyp99 <acoyp1999@gmail.com>
2020-06-01 00:23:19 -0500

    VM-vsphere-provider

--- vSphere-Provider/main.tf
+++ vSphere-Provider/main.tf
@@ -24,7 +24,7 @@ data "vsphere_resource_pool" "pool" {
 }
 
 data "vsphere_network" "network" {
-  name          = "VM Network"
+  name          = "Management Network"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
