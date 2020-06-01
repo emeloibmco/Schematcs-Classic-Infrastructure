9783095bc63c50aa578e159fa6e64ae30ef1aa76
acoyp99 <acoyp1999@gmail.com>
2020-06-01 02:52:51 -0500

    VM-vsphere-provider

--- null-resource/variables.tf
+++ null-resource/variables.tf
@@ -2,3 +2,6 @@ variable "ibmcloud_api_key" {
   description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys"
 }
 
+variable "private_key" {
+  
+}
