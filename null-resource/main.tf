fbaf0e32342aadfeca6a57a6fda0b3b55dc474bb
acoyp99 <acoyp1999@gmail.com>
2020-05-31 21:48:05 -0500

    VM-vsphere-provider

--- null-resource/main.tf
+++ null-resource/main.tf
@@ -11,15 +11,11 @@ resource "null_resource" "win_vm" {
      
   }
   provisioner "local-exec" {
+    command = "ls"
+     
+  }
+  provisioner "local-exec" {
     command = "chmod +x MotionPro_Linux_Ubuntu_x64_v1.2.6.sh"
-     
-  }
-  provisioner "local-exec" {
-    command = "printf '%s\n' '#!/bin/bash' 'exit 0' | tee -a /etc/rc.local"
-     
-  }
-  provisioner "local-exec" {
-    command = "chmod +x /etc/rc.local"
      
   }
   provisioner "local-exec" {
