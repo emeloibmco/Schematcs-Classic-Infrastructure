635301b54063cf0545052fa148c366ebf9559d3e
acoyp99 <acoyp1999@gmail.com>
2020-06-01 11:32:46 -0500

    VM-vsphere-provider

--- local-exec/main.tf
+++ local-exec/main.tf
@@ -2,8 +2,6 @@ resource "null_resource" "keys" {
 
 
   provisioner "local-exec" {
-    inline = [
-      "ssh-keygen -m PEM -t rsa -f 'NOMBRE_DE_LA_LLAVE'"
-    ]
+    command = "ssh-keygen -m PEM -t rsa -f 'NOMBRE_DE_LA_LLAVE'"
   }
 }
