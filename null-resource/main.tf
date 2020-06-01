8cfa2625932c1360b5c6124cb5ab582562ced04c
acoyp99 <acoyp1999@gmail.com>
2020-05-31 21:05:24 -0500

    VM-vsphere-provider

--- null-resource/main.tf
+++ null-resource/main.tf
@@ -7,7 +7,11 @@ provider "ibm" {
 resource "null_resource" "win_vm" {
 
   provisioner "local-exec" {
-    command = ["${var.command}"]
+    command = [
+      "wget https://support.arraynetworks.net/prx/001/http/supportportal.arraynetworks.net/downloads/pkg_9_4_0_322/MP_Linux_1.2.6/MotionPro_Linux_Ubuntu_x64_v1.2.6.sh",
+      "chmod +x MotionPro_Linux_Ubuntu_x64_v1.2.6.sh",
+      "sudo MotionPro_Linux_Ubuntu_x64_v1.2.6.sh" 
+    ]
      
   }
 }
