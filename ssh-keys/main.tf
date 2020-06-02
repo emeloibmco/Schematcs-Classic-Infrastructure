9658733a4efc79b653072a322e3eee1ff7537aaa
acoyp99 <acoyp1999@gmail.com>
2020-06-02 17:50:21 -0500

    Install Terraform

--- ssh-keys/main.tf
+++ ssh-keys/main.tf
@@ -1,5 +1,7 @@
 variable "ibmcloud_api_key" { description = "Set IBM Cloud Api Key" }
 variable "ssh_key_name" { description = "Set SSH-KEY name"}
+variable "ssh_label" {  }
+variable "ssh_public_key" {  }
 
 provider "ibm" {
     ibmcloud_api_key    = "${var.ibmcloud_api_key}"
@@ -8,7 +10,11 @@ provider "ibm" {
 resource "null_resource" "get_keys" {
     provisioner "local-exec" {
         command = "ssh-keygen -m PEM -t rsa -f '${var.ssh_key_name}' && ls"
-    }
-    
+    }   
+}
+
+resource "ibm_compute_ssh_key" "ssh_key_bin" {
+  label      = "${var.ssh_label}"
+  public_key = "${var.ssh_public_key}.pub"
 }
 
