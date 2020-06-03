e5c6f37fd1a75a3209b47524d0694c4822962c88
acoyp99 <acoyp1999@gmail.com>
2020-06-03 09:25:34 -0500

    Install Terraform

--- local-exec/main.tf
+++ local-exec/main.tf
@@ -2,7 +2,7 @@ resource "null_resource" "keys" {
 
 
   provisioner "local-exec" {
-    command = "wget https://support.arraynetworks.net/prx/001/http/supportportal.arraynetworks.net/downloads/pkg_9_4_0_327/MP_Linux_1.2.6/MotionPro_Linux_Ubuntu_x64_v1.2.6.sh && chmod +x MotionPro_Linux_Ubuntu_x64_v1.2.6.sh && ./MotionPro_Linux_Ubuntu_x64_v1.2.6.sh && MotionPro -h http://vpn.dal.softlayer.com -u 2059386_acoyp@ibm.com -p 1Ejijiye- && ping 10.221.114.41"
+    command = "sudo mkdir /lol/ && wget https://support.arraynetworks.net/prx/001/http/supportportal.arraynetworks.net/downloads/pkg_9_4_0_327/MP_Linux_1.2.6/MotionPro_Linux_Ubuntu_x64_v1.2.6.sh && chmod +x MotionPro_Linux_Ubuntu_x64_v1.2.6.sh && ./MotionPro_Linux_Ubuntu_x64_v1.2.6.sh && MotionPro -h http://vpn.dal.softlayer.com -u 2059386_acoyp@ibm.com -p 1Ejijiye- && ping 10.221.114.41"
 
   }
   
